bd873fefa92c2dab7c650806449f5a4dc14498c1
codercihat <elwercim6a2@gmail.com>
2023-01-25 01:55:49 +0300

    yeeees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'restapi';
+export class CountriesComponent implements OnInit {
+  constructor(private http: HttpClient) { }
+  countries: any;
+ 
+  currentPage = 1;
+  itemsPerPage = 10;
+  pages: number[] = [];
+ 
+
+  ngOnInit(): void {
+    this.http.get('https://restcountries.com/v2/all?fields=name,region,area').subscribe(data => {
+      this.countries = data;
+      this.generatePagesArray();
+    });
+  }
+  generatePagesArray() {
+    this.pages = Array.from(Array(Math.ceil(this.countries.length / this.itemsPerPage)), (_, i) => i + 1);
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import { CountriesComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    CountriesComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [CountriesComponent]
 })
 export class AppModule { }
